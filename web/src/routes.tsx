03bba8b026f719dbe631e14c2ea98a77bd0ef25c
Renan Albuquerque <prog.renan@gmail.com>
2020-06-07 21:39:50 -0300

    up project

---
+++ web/src/routes.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
+
+import Home from './pages/Home';
+import CreatPoint from './pages/Createpoint';
+
+const Routes = () => {
+    return (
+        <BrowserRouter>
+            <Route component={Home} path="/" exact />
+            <Route component={CreatPoint} path="/create-point" />
+
+        </BrowserRouter>
+    );
+
+}
+
+export default Routes;
